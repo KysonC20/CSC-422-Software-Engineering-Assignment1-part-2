8e0113442616716a41a1527208712757a5369f70
KysonC20 <148891190+KysonC20@users.noreply.github.com>
2026-01-16 12:20:21 -0600

    Update petClass.java

--- petClass.java
+++ petClass.java
@@ -4,35 +4,41 @@ import java.util.ArrayList;
 public class petClass {
     private String name;
     private String type;
-    Scanner Scanner = new Scanner(System.in);
-    ArrayList<petClass> petArray = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
+    private static ArrayList<petClass> petArray = new ArrayList<>();
 
     public petClass(String name, String type) {
         this.name = name;
         this.type = type;
     }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() {return name;}
+    public String getType() {return type;}
 
-    public String getType() {
-        return type;
-    }
-
+    @Override
     public String toString() {
         return "Pet(name: " + name + ", Type: " + type + ")";
     }
 
-    private void addPet(String input){
+    public static void addPet(){
         System.out.println("Enter the Pets name: ");
-        input = Scanner.nextLine();
-        System.out.println("Pet was added: " + input);
+        String name = scanner.nextLine();
+        System.out.println("Enter the pets type, Ex.dog or cat: ");
+        String type = scanner.nextLine();
+
+        petClass PetClass = new petClass(name, type);
+        petArray.add(PetClass);
+
+        System.out.println("Pet was added: " + PetClass);
     }
 
-    private void displayPet(){
+    public static void displayPet(){
         if (petArray.isEmpty()) {
             System.out.println("No pets currently in array");
+        }
+        System.out.println("\nList of pets:");
+        for (int i = 0; i <petArray.size(); i++) {
+            System.out.println(i + 1 + ": " + petArray.get(i));
         }
     }
 }
